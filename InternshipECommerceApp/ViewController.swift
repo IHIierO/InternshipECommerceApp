d6444688898c0fbb7efffcc72dd0ca6936cf1c91
Artem <t-e-m-a_@mail.ru>
2023-03-13 11:53:12 +0300

    Initial Commit

---
+++ InternshipECommerceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InternshipECommerceApp
+//
+//  Created by Artem Vorobev on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
